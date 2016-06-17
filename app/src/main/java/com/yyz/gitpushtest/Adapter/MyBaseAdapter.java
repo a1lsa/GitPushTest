f187cc0b7e8578caaf0273369b7cf5bb8d5f0eb1
a1lsa <562486918@qq.com>
2016-06-17 16:32:26 +0800

    添加了一个adapter包，并写了BaseAdapter

---
+++ app/src/main/java/com/yyz/gitpushtest/Adapter/MyBaseAdapter.java
@@ -0,0 +1,29 @@
+package com.yyz.gitpushtest.Adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mry on 2016/6/17.
+ */
+public class MyBaseAdapter extends android.widget.BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
